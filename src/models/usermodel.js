99ba23a20880588814cb3d9c340708b3693559d2
Murali Mohan Reddy <nagamuralimohan12@gmail.com>
2025-06-30 10:25:01 +0530

    USer Scheam Completed

--- src/models/usermodel.js
+++ src/models/usermodel.js
@@ -1,4 +1,6 @@
 import mongoose from "mongoose";
+import bcrypt from "bcrypt";
+import jwt from "jsonwebtoken";
 
 const userSchema = new mongoose.Schema(
   {
@@ -43,7 +45,44 @@ const userSchema = new mongoose.Schema(
   }
 );
 
+userSchema.pre("save", async function (next) {
+    if(!this.isModified("password")) return next();
 
+    this.password = await bcrypt.hash(this.password,10);
+    next();
+})
 
+userSchema.methods.isPasswordCorrect = async function (password) {
+    return await bcrypt.compare(password, this.password)
+}
+
+userSchema.methods.generateAccessToken = function () {
+   const token = jwt.sign(
+        {
+            _id: this._id,
+            email: this.email,
+            username: this.username,
+            fullName : this.fullName
+        },
+        process.env.ACCESS_TOKEN_SECRET,{
+            expiresIn: process.env.ACCESS.TOKEN.EXPIRY
+        }
+    )
+    console.log("token : ",token);
+    return token
+}
+
+userSchema.methods.generateRefreshToken = function () {
+   const token =  jwt.sign(
+        {
+            _id: this._id
+        },
+        process.env.REFRESH_TOKEN_SECRET,{
+            expiresIn: process.env.REFRESH_TOKEN_EXPIRY
+        }
+    )
+    console.log("token : ",token);
+    return token
+}
 
 export default User = mongoose.model("User", userSchema);
